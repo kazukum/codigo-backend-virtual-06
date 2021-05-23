b472b1188e0aa028fb4fc92cdb9c2d389ef946e0
unknown <cesar_akt@hotmail.com>
2021-05-23 14:53:04 -0500

    inicio semana01, definicion de variables, incremento, operadores artimeticos y de asignacion

---
+++ Dia1/02-impresion-pantalla.py
@@ -0,0 +1,15 @@
+estacion = "otoño"
+
+# concatenar varios valores, siempre y cuando sean todos STRING y pondran un espacio entre valor y valor
+print("la estacion es:", estacion, ".")
+
+
+alumnos = 22
+grupo = 6
+print("los alumnos son :{} y son del grupo {} y todos son divertidos".format(alumnos, grupo))
+
+print("los alumnos del grupo {1} son en total {0}".format(alumnos, grupo))
+
+
+# para que python ejecute todo tienes que poner una f al comienzo y tiene q estar en llaves la variable
+print(f"los alumnos son en total {alumnos}")
