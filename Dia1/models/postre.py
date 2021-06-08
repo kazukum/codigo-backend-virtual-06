5fcbafe6480119e3695897d6edf10be2cd7724c4
unknown <cesar_akt@hotmail.com>
2021-06-08 13:09:03 -0500

    subiendo la aplicacion

--- Dia1/models/postre.py
+++ Dia1/models/postre.py
@@ -18,6 +18,36 @@ class PostreModel(base_de_datos.Model):
     postrePorcion = Column(name="porcion",type_=types.String(length=25))
 
     preparaciones = orm.relationship("PreparacionModel", backref="preparacionPostre", lazy =True)
+
+    recetas = orm.relationship("RecetaModel",backref="recetaPostre")
+
     def __init__(self,nombre,porcion):
         self.postreNombre = nombre
         self.postrePorcion = porcion
+
+
+    def __str__(self):
+        return "El postre es %s" % self.postreNombre
+
+
+    def save(self):
+
+        base_de_datos.session.add(self)
+
+        base_de_datos.session.commit()
+        
+        # metodo que sirve para cerrar la secion de la BD
+        # no puedes volver a realizar una peticion a menos que te devuelvas a conectar 
+
+        # base_de_datos.session.close()
+
+    def json(self):
+        return{
+            "id": self.postreId,
+            "postreNombre":self.postreNombre,
+            "postrePorcion":self.postrePorcion
+        }
+
+    def delete(self):
+        base_de_datos.session.delete(self)
+        base_de_datos.session.commit()
